98865b113ba1d9ea9fc12f86db247d36db71e7ad
Tada, Tadashi <t@tdtds.jp>
2020-01-18 14:47:38 +0900

    first commit from bundle gem template

---
+++ lib/fluent/plugin/wmi.rb
@@ -0,0 +1,10 @@
+require "fluent/plugin/wmi/version"
+
+module Fluent
+  module Plugin
+    module Wmi
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/wmi/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Wmi
+      VERSION = "0.1.0"
+    end
+  end
+end
