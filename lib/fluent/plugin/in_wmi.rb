b2f531f6aec87784c24bdacaba64df24f50ff3e0
Tada, Tadashi <t@tdtds.jp>
2020-01-26 20:29:40 +0900

    add skeleton of standard methods of plugin

--- lib/fluent/plugin/in_wmi.rb
+++ lib/fluent/plugin/in_wmi.rb
@@ -6,5 +6,17 @@ require "fluent/plugin/input"
 module Fluent::Plugin
   class WmiInput < Fluent::Plugin::Input
     Fluent::Plugin.register_input("wmi", self)
+
+    def configure(conf)
+      super
+    end
+
+    def start
+      super
+    end
+
+    def shutdown
+      super
+    end
   end
 end
