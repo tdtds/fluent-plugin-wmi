45836ee582f0015954c9fcecdf56b2e718caec5b
TADA Tadashi <t@tdtds.jp>
2020-02-05 16:12:24 +0900

    emit event in specified interval time
    * implement timer by plugin helper
    * test configurations
    
    Signed-off-by: TADA Tadashi <t@tdtds.jp>

--- lib/fluent/plugin/in_wmi.rb
+++ lib/fluent/plugin/in_wmi.rb
@@ -7,25 +7,34 @@ require "wmi-lite"
 module Fluent::Plugin
   class WmiInput < Fluent::Plugin::Input
     Fluent::Plugin.register_input("wmi", self)
+    helpers :timer
 
     desc "The value is the tag assigned to the generated events."
     config_param :tag, :string
-
     desc "The value is the instance name of WMI."
     config_param :class_name, :string
+    desc "Determine the rate to emit metrics as events."
+    config_param :interval, :time, default: 10
 
     def configure(conf)
       super
-
       raise Fluent::ConfigError, "class_name is required." unless @class_name
+      begin
+        WmiLite::Wmi.new.instances_of(@class_name)
+      rescue WmiLite::WmiException
+        raise Fluent::ConfigError, "#{@class_name} is invalid class_name."
+      end
+      raise Fluent::ConfigError, "interval must be large than 1." if @interval < 1
     end
 
     def start
       super
-      wmi = WmiLite::Wmi.new
-      time = Fluent::Engine.now
-      wmi.instances_of(@class_name).each do |instance|
-        router.emit(@tag, time, instance.property_map)
+      timer_execute(:wmi_timer, @interval) do
+        wmi = WmiLite::Wmi.new
+        time = Fluent::Engine.now
+        wmi.instances_of(@class_name).each do |instance|
+          router.emit(@tag, time, instance.property_map)
+        end
       end
     end
 
